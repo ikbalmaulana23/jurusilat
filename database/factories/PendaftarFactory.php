15d3658110234661bf99ade1ab53b08b848b2b17
Ikbal Maulana <86029687+ajobale@users.noreply.github.com>
2024-07-30 10:11:02 +0700

    Pendaftar Seeder

---
+++ database/factories/PendaftarFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pendaftar>
+ */
+class PendaftarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name(),
+            'jen_kelamin' => $this->faker->randomElement(['Laki-laki', 'Perempuan']),
+            'tpt_lahir' => $this->faker->city(),
+            'tgl_lahir' => $this->faker->date(),
+            'tb' => $this->faker->numberBetween(150, 200) . ' cm',
+            'bb' => $this->faker->numberBetween(50, 100) . ' kg',
+            'kelas' => $this->faker->randomElement(['A', 'B', 'C', 'D', 'E']),
+            'asal_sekolah' => $this->faker->company(),
+            'kategori_tanding' => $this->faker->word(),
+            'golongan' => $this->faker->word(),
+            'kode_gr' => $this->faker->bothify('GR-####'),
+            'kelas_tanding_FK' => $this->faker->word(),
+            'kontingen' => $this->faker->state(),
+            'foto' => $this->faker->imageUrl(),
+            'ktp' => $this->faker->numerify('KTP####'),
+            'akta_lahir' => $this->faker->numerify('AK####'),
+            'ijazah' => $this->faker->numerify('IJ####'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\PendaftarSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        $this->call([
+            PendaftarSeeder::class,
+        ]);
     }
 }
